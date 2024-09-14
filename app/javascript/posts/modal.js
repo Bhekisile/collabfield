739fa1b4461e953b9b4193d865b6b4417e2d3980
Bhekisile <bhekisilejozi@gmail.com>
2024-09-14 14:53:00 +0200

    Add a popup window to show a full posts content

--- app/javascript/posts/modal.js
+++ app/javascript/posts/modal.js
@@ -1,4 +1,5 @@
-$(document).on('turbolinks:load', function() {
+// $(document).on('turbolinks:load', function() {
+  document.addEventListener('turbo:load', function() {
   // when a post is clicked, show its full content in a modal window
   $("body").on( "click", ".single-post-card, .single-post-list", function() {
     var posted_by = $(this).find('.post-content .posted-by').html();
