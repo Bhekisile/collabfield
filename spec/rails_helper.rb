f922a53d95883325a8ec378aaa07d28a72461ad1
Bhekisile <bhekisilejozi@gmail.com>
2024-09-19 12:49:51 +0200

    Add request specs for a new post template

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -17,6 +17,8 @@ require 'capybara/rspec'
 # Capybara.javascript_driver = :poltergeist
 Capybara.javascript_driver = :selenium_chrome_headless
 Capybara.server = :puma
+
+include Warden::Test::Helpers
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
