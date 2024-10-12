fc47ee8ed6a6f99b53812070b5b890d450ab984c
Bhekisile <bhekisilejozi@gmail.com>
2024-10-12 19:57:26 +0200

    Create a remove-more-messages-link inside shared/load-more-messages/window

--- app/controllers/private/messages_controller.rb
+++ app/controllers/private/messages_controller.rb
@@ -5,6 +5,7 @@ class Private::MessagesController < ActionController::Base
     get_messages('private', 10)
     @user = current_user
     @is_messenger = params[:is_messenger]
+
     respond_to do |format|
       format.js { render partial: 'private/messages/load_more_messages' }
     end
