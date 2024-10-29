495d9e2f64ea439855746d7c0f2b7be2f236638c
Bhekisile <bhekisilejozi@gmail.com>
2024-10-29 13:11:22 +0200

    Create _select_user.html.erb and _messages_list.html.erb inside   group/conversations/conversation

--- spec/helpers/group/conversations_helper_spec.rb
+++ spec/helpers/group/conversations_helper_spec.rb
@@ -7,9 +7,9 @@ RSpec.describe Group::ConversationsHelper, type: :helper do
     let(:user) { create(:user) }
     before(:each) do
       create(:contact,
-              user_id: current_user.id,
-              contact_id: user.id,
-              accepted: true)
+             user_id: current_user.id,
+             contact_id: user.id,
+             accepted: true)
     end
 
     it 'a user is not included in a list' do
@@ -23,6 +23,22 @@ RSpec.describe Group::ConversationsHelper, type: :helper do
       conversation.users << current_user
       helper.stub(:current_user).and_return(current_user)
       expect(helper.add_people_to_group_conv_list(conversation)).to include user
+    end
+  end
+
+  context '#load_group_messages_partial_path' do
+    let(:conversation) { create(:group_conversation) }
+    it "returns load_messages partial's path" do
+      create_list(:group_message, 2, conversation_id: conversation.id)
+      expect(helper.load_group_messages_partial_path(conversation)).to eq(
+        'group/conversations/conversation/messages_list/link_to_previous_messages'
+      )
+    end
+
+    it "returns an empty partial's path" do
+      expect(helper.load_group_messages_partial_path(conversation)).to eq(
+        'shared/empty_partial'
+      )
     end
   end
 end
