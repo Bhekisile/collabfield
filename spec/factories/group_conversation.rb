aca80412af5c28c44489924fba44e09e92f7b468
Bhekisile <bhekisilejozi@gmail.com>
2024-10-29 11:39:04 +0200

    Edit Group::Conversation and Group::Message models

--- spec/factories/group_conversation.rb
+++ spec/factories/group_conversation.rb
@@ -1,6 +1,27 @@
+# FactoryBot.define do
+#   factory :group_conversation, class: 'Group::Conversation' do
+#     association :users, factory: :user
+
+#     factory :group_conversation_with_messages do
+#       transient do
+#         messages_count { 1 }
+#       end
+
+#       after(:create) do |group_conversation, evaluator|
+#         create_list(:group_message, evaluator.messages_count,
+#                     conversation: group_conversation)
+#       end
+#     end
+#   end
+# end
+
 FactoryBot.define do
   factory :group_conversation, class: 'Group::Conversation' do
-    association :users, factory: :user
+    # Define the relationship correctly with multiple users
+    after(:create) do |group_conversation|
+      # Add multiple users to the conversation
+      group_conversation.users << create_list(:user, 2)
+    end
 
     factory :group_conversation_with_messages do
       transient do
--- spec/factories/group_message.rb
+++ spec/factories/group_message.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :group_message, class: 'Group::Message' do
     content { 'a' * 20 }
     association :conversation, factory: :group_conversation
-    user
+    association :user, factory: :user
   end
 end
