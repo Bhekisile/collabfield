a2837a4f94d7833cfaf1c3d606fff7adc40f5e6a
Bhekisile <bhekisilejozi@gmail.com>
2024-12-20 10:19:03 +0200

    Fix rubocop error

--- app/helpers/shared/messages_helper.rb
+++ app/helpers/shared/messages_helper.rb
@@ -4,7 +4,7 @@ module Shared::MessagesHelper
   end
 
   def replace_link_to_group_messages_partial_path
-    'group/messages/load_more_messages/window/replace_link_to_messages'   
+    'group/messages/load_more_messages/window/replace_link_to_messages'
   end
 
   # if there are no previous messages
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+# rubocop:disable Metrics/BlockLength
 ActiveRecord::Schema[7.1].define(version: 20_241_021_145_810) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
@@ -97,3 +98,4 @@ ActiveRecord::Schema[7.1].define(version: 20_241_021_145_810) do
 
   add_foreign_key 'private_messages', 'users'
 end
+# rubocop:enable Metrics/BlockLength
